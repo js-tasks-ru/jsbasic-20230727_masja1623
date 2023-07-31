82af04e16af8443893bed7f144fdf1a5fcc6bce0
marynapashkevych <masja1623@gmail.com>
2023-07-31 23:38:03 +0200

    fix spaces

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -2,6 +2,7 @@ function factorial(n) {
   let result = 1;
 
   if (n === 0 || n === 1) {
+
     return result;
   }
   if (n > 1) {
@@ -9,18 +10,20 @@ function factorial(n) {
 //   for (let i = n; i >= 1; i--) {
 //     answer =  answer * i
 //   }
-// return answer
+//
+// return answer;
 
     // DESC: second method
-    let i = n
+    let i = n;
     while (i >=1) {
       result =  result * i
       i--;
     }
+
     return result;
   }
   else {
+
     return ('Error, enter the correct value of n');
   }
 }
-
