c8475b3aa640dae1a00dddc5d0d5ee0769ec7de0
marynapashkevych <masja1623@gmail.com>
2023-07-31 23:34:47 +0200

    add several methods to calculate the factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,26 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+
+  if (n === 0 || n === 1) {
+    return result;
+  }
+  if (n > 1) {
+    // DESC: fist method
+//   for (let i = n; i >= 1; i--) {
+//     answer =  answer * i
+//   }
+// return answer
+
+    // DESC: second method
+    let i = n
+    while (i >=1) {
+      result =  result * i
+      i--;
+    }
+    return result;
+  }
+  else {
+    return ('Error, enter the correct value of n');
+  }
 }
+
