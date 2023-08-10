c69d87205cedb01c432f47f0f8a0443f6774597d
Maryna Pashkevych <maryna.pashkevych1@gmail.com>
2023-08-10 21:28:11 +0200

    add math max and math min methods

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str.split(' ').filter((value) => !isNaN(parseFloat(value))).map(value => parseFloat(value))
+
+  let result = {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  }
+  return result
 }
