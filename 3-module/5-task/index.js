32e3461b450b750d0cd97b62b75161e13168a9bf
Maryna Pashkevych <maryna.pashkevych1@gmail.com>
2023-08-10 22:01:25 +0200

    fix mor typo

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,9 +1,13 @@
 function getMinMax(str) {
-  let numbers = str.split(' ').filter((value) => !isNaN(parseFloat(value))).map(value => parseFloat(value))
+  let numbers = str
+    .split(' ')
+    .filter((value) => !isNaN(parseFloat(value)))
+    .map(value => parseFloat(value));
 
   let result = {
     min: Math.min(...numbers),
     max: Math.max(...numbers)
-  }
-  return result
+  };
+
+  return result;
 }
